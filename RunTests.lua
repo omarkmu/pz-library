b7386b99c46365411d92ec4f294f79701c876cf4
Omar <omarkmu@gmail.com>
2024-03-24 21:37:28 -0500

    Delete obsolete RunTests.lua

--- RunTests.lua
+++
@@ -1,8 +0,0 @@
-local test = require 'test'
-
-if test.isMain() then
-    test.run({
-        require('tests/InterpolatorTest'),
-        require('tests/TableUtilsTest'),
-    }, { args = { ... } })
-end
